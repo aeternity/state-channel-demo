929647b9f91c8677c13806e8cb1c0c030def6f66
martinkaintas <martinkaintas@gmail.com>
2022-06-22 21:52:40 +0300

    feat: add front skeleton code

---
+++ front/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Aeternity logo" src="./assets/logo.png" :style="{width: '100px'}" />
+  <HelloWorld msg="State Channel Demo Front" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
