ba53e3e3eac14392cf8ce0cdead069dfb96ea1a4
Kalovelo <apostoloskalovelonis@gmail.com>
2022-06-23 16:09:44 +0300

    backend/feat: initiate backend service

---
+++ nodejs/src/services/bot/bot.service.ts
@@ -0,0 +1,71 @@
+import { EncodedData } from '@aeternity/aepp-sdk/es/utils/encoder';
+import BigNumber from 'bignumber.js';
+import { MemoryAccount, generateKeyPair, Channel } from '@aeternity/aepp-sdk';
+import { getSdk, BaseAe, networkId } from '../sdk/sdk.service.development';
+import { waitForChannel } from '../sdk/sdk.service';
+
+const channelPool = new WeakSet<Channel>();
+
+const channelConfig = {
+  url: process.env.WS_URL ?? 'ws://localhost:3014/channel',
+  pushAmount: 3,
+  initiatorAmount: new BigNumber('1e2'),
+  responderAmount: new BigNumber('1e2'),
+  channelReserve: 0,
+  ttl: 10000,
+  host: 'localhost',
+  port: 3001,
+  lockPeriod: 1,
+  debug: false,
+  role: 'initiator',
+};
+
+function removeChannel(channel: Channel) {
+  channelPool.delete(channel);
+}
+function addChannel(channel: Channel) {
+  channelPool.add(channel);
+}
+
+async function fundAccount(account: EncodedData<'ak'>) {
+  const sdk = await getSdk();
+  return sdk.spend(1e18, account);
+}
+
+function registerEvents(channel: Channel) {
+  channel.on('statusChanged', (status) => {
+    if (status === 'disconnected') {
+      removeChannel(channel);
+    }
+
+    if (status === 'open') {
+      addChannel(channel);
+    }
+  });
+}
+
+export async function generateGameSession(playerKey: EncodedData<'ak'>) {
+  const bot = await BaseAe({
+    accounts: [new MemoryAccount({ keypair: generateKeyPair() })],
+    networkId,
+  });
+
+  const initiatorId = await bot.address();
+  const responderId = playerKey;
+
+  await fundAccount(initiatorId);
+  await fundAccount(responderId);
+
+  const channel = await Channel.initialize({
+    ...channelConfig,
+    initiatorId,
+    responderId,
+    role: 'initiator',
+    sign: (_tag, tx) => bot.signTransaction(tx),
+  });
+
+  registerEvents(channel);
+
+  await Promise.all([waitForChannel(channel)]);
+  return channel;
+}
---
+++ nodejs/src/services/sdk/sdk.service.development.ts
@@ -0,0 +1,66 @@
+import {
+  AeSdk,
+  Node,
+  MemoryAccount,
+  generateKeyPair,
+  AccountBase,
+} from '@aeternity/aepp-sdk';
+import { EncodedData } from '@aeternity/aepp-sdk/es/utils/encoder';
+
+export const url = process.env.NODE_URL ?? 'http://localhost:3013';
+export const compilerUrl = process.env.COMPILER_URL ?? 'http://localhost:3080';
+export const publicKey = (process.env.PUBLIC_KEY as EncodedData<'ak'>)
+  ?? 'ak_2dATVcZ9KJU5a8hdsVtTv21pYiGWiPbmVcU1Pz72FFqpk9pSRR';
+const secretKey = process.env.SECRET_KEY
+  ?? 'bf66e1c256931870908a649572ed0257876bb84e3cdf71efb12f56c7335fad54d5cf08400e988222f26eb4b02c8f89077457467211a6e6d955edb70749c6a33b';
+export const networkId = process.env.NETWORK_ID ?? 'ae_devnet';
+export const ignoreVersion = process.env.IGNORE_VERSION === 'true';
+export const genesisAccount = new MemoryAccount({
+  keypair: { publicKey, secretKey },
+});
+export const account = generateKeyPair();
+
+export const BaseAe = async (
+  params: {
+    accounts?: AccountBase[];
+    withoutGenesisAccount?: boolean;
+    networkId?: string;
+    waitMined?: boolean;
+    innerTx?: boolean;
+  } = {},
+): Promise<AeSdk> => {
+  const sdk = new AeSdk({
+    ...params,
+    compilerUrl,
+    ignoreVersion,
+    nodes: [{ name: 'test', instance: new Node(url, { ignoreVersion }) }],
+    _expectedMineRate: 1000,
+    _microBlockCycle: 300,
+  });
+  await Promise.all(
+    [
+      ...(params.accounts ?? []),
+      ...(params.withoutGenesisAccount === true ? [] : [genesisAccount]),
+    ].map(async (account, i) => sdk.addAccount(account, { select: i === 0 })),
+  );
+  return sdk;
+};
+
+export const spendPromise = (async () => {
+  const ae = await BaseAe({ networkId, withoutGenesisAccount: false });
+  await ae.awaitHeight(2);
+  await ae.spend(1e26, account.publicKey);
+})();
+
+export async function getSdk({
+  withoutAccount,
+}: { withoutAccount?: boolean } = {}): Promise<AeSdk> {
+  await spendPromise;
+
+  return BaseAe({
+    ...(withoutAccount === true
+      ? { withoutGenesisAccount: true }
+      : { accounts: [new MemoryAccount({ keypair: account })] }),
+    networkId,
+  });
+}
