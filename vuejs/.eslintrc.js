781740eb73e9d181df1dcabb19a41f58aa6f33a9
martinkaintas <martinkaintas@gmail.com>
2022-06-23 13:55:01 +0300

    feat: configure eslint and prettier

---
+++ vuejs/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+    'prettier',
+    '@vue/typescript/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
