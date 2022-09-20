fa67546b7ef6a63aba09e4a109c9eb060ab96ceb
Kalovelo <apostoloskalovelonis@gmail.com>
2022-09-20 12:39:39 +0300

    feat: seperate nodes per env config

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,15 +4,15 @@ import { route } from './route';
 
 export const app = express();
 
-const corsOptions = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'testnet'
-  ? null
-  : {
+const corsOptions = process.env.NODE_ENV === 'production'
+  ? {
     origin: [
       /\.aeternity\.com\/?$/,
       /\.aepps\.com\/?$/,
       /.*\.aepps\.com\/rock-paper-scissors\/?$/,
     ],
-  };
+  }
+  : null;
 
 app.use(cors(corsOptions));
 app.use(express.json());
