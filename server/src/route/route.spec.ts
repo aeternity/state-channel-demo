0f3a42b6fca889f51ffa883e29f6f365cc8f1218
Kalovelo <apostoloskalovelonis@gmail.com>
2022-09-22 11:19:00 +0300

    feat: add debug status endpoint

--- server/src/route/route.spec.ts
+++ server/src/route/route.spec.ts
@@ -1,19 +1,118 @@
+import { Channel } from '@aeternity/aepp-sdk';
 import { ChannelOptions } from '@aeternity/aepp-sdk/es/channel/internal';
+import { ContractInstance } from '@aeternity/aepp-sdk/es/contract/aci';
+import { Encoded } from '@aeternity/aepp-sdk/es/utils/encoder';
 import supertest from 'supertest';
-import { app } from '../app';
 import { mockChannel } from '../../test';
+import { app } from '../app';
+import env from '../env';
 import { MUTUAL_CHANNEL_CONFIGURATION } from '../services/bot';
+import { addGameSession, removeGameSession } from '../services/bot/bot.service';
+import { ContractService } from '../services/contract';
+
+const config: {
+  address: Encoded.AccountAddress;
+  port: number;
+  host: string;
+} = {
+  address: 'ak_2fnsAzkXfvWKZJXx2bkPSWMtpBpfEXFhJpL9ThFLWMxQ7G9wag',
+  port: 8000,
+  host: 'localhost',
+};
+
+const deployContractSpy = jest.spyOn(ContractService, 'deployContract');
+
+describe('/status', () => {
+  async function fetchStatus() {
+    return supertest(app).get('/status').set('Accept', 'application/json');
+  }
+
+  it('returns 0 as default values', async () => {
+    const res = await fetchStatus();
+    const currentDate = new Date();
+
+    expect(res.status).toBe(200);
+    expect(
+      currentDate.getMilliseconds()
+        - new Date(res.body.runningSince as number).getMilliseconds(),
+    ).toBeLessThan(1000);
+    expect(res.body).toEqual({
+      channelsOpenCurrently: 0,
+      channelsInitialized: 0,
+      channelsOpened: 0,
+      runningSince: expect.any(String),
+      env: {
+        ...env.development,
+      },
+    });
+  });
+
+  it('increments channels', async () => {
+    await supertest(app)
+      .post('/open')
+      .send(config)
+      .set('Accept', 'application/json');
+
+    const initialRes = await fetchStatus();
+
+    expect(initialRes.body.channelsInitialized).toBe(1);
+    expect(initialRes.body.channelsOpened).toBe(0);
+    expect(initialRes.body.channelsOpenCurrently).toBe(0);
+
+    deployContractSpy.mockResolvedValue({
+      instance: {} as ContractInstance,
+      address: 'ct_2fnsAzkXfvWKZJXx2bkPSWMtpBpfEXFhJpL9ThFLWMxQ7G9wag',
+    });
+    await addGameSession(
+      {
+        balances: jest.fn,
+        state: jest.fn,
+        poi: jest.fn,
+      } as unknown as Channel,
+      {
+        initiatorId: config.address,
+        responderId: 'ak_2fnsAzkXfvWKZJXx2bkPSWMtpBpfEXFhJpL9ThFLWMxQ7G9waz',
+      } as unknown as ChannelOptions,
+    );
+
+    const res = await fetchStatus();
+
+    const currentDate = new Date();
+
+    expect(res.status).toBe(200);
+    expect(
+      currentDate.getMilliseconds()
+        - new Date(res.body.runningSince as number).getMilliseconds(),
+    ).toBeLessThan(1000);
+    expect(res.body).toEqual({
+      channelsOpenCurrently: 1,
+      channelsInitialized: 1,
+      channelsOpened: 1,
+      runningSince: expect.any(String),
+      env: {
+        ...env.development,
+      },
+    });
+  });
+
+  it('decrements channels', async () => {
+    removeGameSession(config.address);
+
+    const res = await supertest(app)
+      .get('/status')
+      .set('Accept', 'application/json');
+
+    expect(res.body.channelsOpenCurrently).toBe(0);
+    expect(res.body.channelsOpened).toBe(1);
+    expect(res.body.channelsInitialized).toBe(1);
+  });
+});
 
 describe('/open', () => {
   jest.setTimeout(15000);
   mockChannel();
 
   it('retrieves channel configuration', async () => {
-    const config = {
-      address: 'ak_2fnsAzkXfvWKZJXx2bkPSWMtpBpfEXFhJpL9ThFLWMxQ7G9wag',
-      port: 8000,
-      host: 'localhost',
-    };
     const res = await supertest(app)
       .post('/open')
       .send(config)
