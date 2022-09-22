ae876931630339ab75c5387ed7eb29aa71a5b5a8
Kalovelo <apostoloskalovelonis@gmail.com>
2022-09-22 11:19:00 +0300

    feat: add debug status endpoint

--- server/src/services/sdk/sdk.constants.ts
+++ server/src/services/sdk/sdk.constants.ts
@@ -9,15 +9,17 @@ if (!Object.keys(env).includes(ENVIRONMENT)) {
   throw new Error(`Environment ${ENVIRONMENT} is not defined in env/index.ts`);
 }
 
-export const WEBSOCKET_URL = env[ENVIRONMENT]?.WS_URL;
-export const NODE_URL = env[ENVIRONMENT]?.NODE_URL;
-export const COMPILER_URL = env[ENVIRONMENT]?.COMPILER_URL;
-export const NETWORK_ID = env[ENVIRONMENT]?.NETWORK_ID;
-export const IGNORE_NODE_VERSION = env[ENVIRONMENT]?.IGNORE_VERSION === 'true';
-export const FAUCET_PUBLIC_ADDRESS = env[ENVIRONMENT]?.FAUCET_PUBLIC_ADDRESS;
+export const ENVIRONMENT_CONFIG = env[ENVIRONMENT];
+
+export const WEBSOCKET_URL = ENVIRONMENT_CONFIG.WS_URL;
+export const {
+  NODE_URL, COMPILER_URL, NETWORK_ID, FAUCET_PUBLIC_ADDRESS,
+} = ENVIRONMENT_CONFIG;
+export const IGNORE_NODE_VERSION = ENVIRONMENT_CONFIG.IGNORE_VERSION === 'true';
 export const IS_USING_LOCAL_NODE = !NODE_URL?.includes('testnet');
-// ! LOCAL NODE USAGE ONLY
-const FAUCET_SECRET_KEY = ENVIRONMENT === 'development' && env[ENVIRONMENT]?.FAUCET_SECRET_KEY;
+// ! LOCAL NODE & DEVELOPMENT NETWORK USAGE ONLY
+const FAUCET_SECRET_KEY = ENVIRONMENT === 'development'
+  && (ENVIRONMENT_CONFIG as typeof env['development'])?.FAUCET_SECRET_KEY;
 export const FAUCET_ACCOUNT = IS_USING_LOCAL_NODE
   ? new MemoryAccount({
     keypair: {
