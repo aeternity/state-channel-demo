39c478b3c85c95d8056395a0c74bc783748ece4e
Kalovelo <apostoloskalovelonis@gmail.com>
2022-10-10 02:08:03 +0300

    feat(client): automatically start new round after 1 second

--- client/main.js
+++ client/main.js
@@ -1,24 +1,7 @@
-import { setMoveSelectionDisability } from './src/js/dom-manipulation/dom-manipulation';
-import { gameChannel } from './src/js/game-channel/game-channel';
-import { Selections } from './src/js/game-channel/game-channel.enums';
-import { initSdk } from './src/js/sdk-service/sdk-service';
 import './src/js/autoplay/autoplay-btn.controler';
+import { initSdk } from './src/js/sdk-service/sdk-service';
+import { handleAppMount } from './src/js/dom-manipulation/dom-manipulation';
+import { gameChannel } from './src/js/game-channel/game-channel';
 
 initSdk();
-
-document.querySelectorAll('.selections button').forEach((button, index) => {
-  button.addEventListener('click', async () => {
-    if (!gameChannel.isOpen && !gameChannel.isOpening) {
-      setMoveSelectionDisability(true);
-      await gameChannel.initializeChannel();
-    }
-    const selection = Object.values(Selections)[index];
-    console.log(selection);
-    if (gameChannel.contractAddress)
-      await gameChannel.setUserSelection(selection);
-    else
-      await gameChannel.pollForContract(() =>
-        gameChannel.setUserSelection(selection)
-      );
-  });
-});
+handleAppMount(gameChannel);
--- client/src/js/game-channel/game-channel.middleware.js
+++ client/src/js/game-channel/game-channel.middleware.js
@@ -1,21 +1,63 @@
 import * as DOMUpdate from '../dom-manipulation/dom-manipulation';
 
+let resetPromise = null;
+
+/**
+ * @param {import("../game-channel/game-channel").GameChannel} gameChannel
+ */
 export function DomMiddleware(gameChannel) {
   if (gameChannel.isOpen) {
     DOMUpdate.setParticipantBalance('user', gameChannel.balances.user);
     DOMUpdate.setParticipantBalance('bot', gameChannel.balances.bot);
+    DOMUpdate.setGameRoundIndex(gameChannel.gameRound.index);
+    DOMUpdate.setStakeAmount(gameChannel.gameRound.stake);
+    DOMUpdate.showGameInfo(false);
+
+    const canReset = gameChannel.isOpen && gameChannel.shouldShowEndScreen;
+    const canClose =
+      gameChannel.isOpen &&
+      !gameChannel.gameRound.userInAction &&
+      gameChannel.channelIsClosing;
+
+    if (canReset || canClose) {
+      DOMUpdate.setResetDisability(false);
+    } else DOMUpdate.setResetDisability(true);
+    if (canClose) {
+      DOMUpdate.setEndGameDisability(false);
+    } else DOMUpdate.setEndGameDisability(true);
+
+    if (gameChannel.gameRound.userSelection != 'none') {
+      DOMUpdate.setMoveStatus('user', 'Waiting for bot...');
+      DOMUpdate.setMoveStatus('bot', '');
+      DOMUpdate.setFinalizedSelection(
+        'user',
+        gameChannel.gameRound.userSelection
+      );
+      DOMUpdate.setMoveSelectionDisability(true);
+    }
+
+    if (gameChannel.gameRound.isCompleted) {
+      DOMUpdate.setMoveStatus('user', '');
+      DOMUpdate.setFinalizedSelection(
+        'bot',
+        gameChannel.gameRound.botSelection
+      );
+      let winner;
+      switch (gameChannel.gameRound.winner) {
+        case gameChannel.channelConfig.initiatorId:
+          winner = 'bot';
+          break;
+        case gameChannel.channelConfig.responderId:
+          winner = 'user';
+          break;
+      }
+      DOMUpdate.colorizeSelections(winner);
+    } else if (gameChannel.gameRound.botSelection == 'none' && !resetPromise) {
+      resetPromise = setTimeout(() => {
+        DOMUpdate.setMoveStatus('bot', 'Waiting for user...');
+        DOMUpdate.resetSelections();
+        resetPromise = null;
+      }, 1000);
+    }
   }
-
-  const canReset = gameChannel.isOpen && gameChannel.shouldShowEndScreen;
-  const canClose =
-    gameChannel.isOpen &&
-    !gameChannel.gameRound.userInAction &&
-    gameChannel.channelIsClosing;
-
-  if (canReset || canClose) {
-    DOMUpdate.setResetDisability(false);
-  } else DOMUpdate.setResetDisability(true);
-  if (canClose) {
-    DOMUpdate.setEndGameDisability(false);
-  } else DOMUpdate.setEndGameDisability(true);
 }
